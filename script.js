014c1a9a48a85e902d40d34afec920f3927479cc
gokulgovinthan <goodgovinthan@gmail.com>
2024-01-09 00:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function isSameType(value1, value2) {
-  if (typeof value1 == typeof value2) {
+  if (typeof value1==typeof value2) {
      return true;	
-  }else
+  }else{
 	  return false;
+  }
 }
 
 // do not change the code below.
