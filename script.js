56180f4fbb8f34fbd06e1fe2f789d304e4aed077
gokulgovinthan <goodgovinthan@gmail.com>
2024-01-09 13:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function isSameType(value1, value2) {
-  if (typeof value1==typeof value2) {
-     return true;	
-  } 
-	if(typeof value1!=typeof value2){
-	  return false;
-  }
+function isInteger(value) {
+    return typeof value === 'number' && isFinite(value) && Math.floor(value) === value;
 }
 
-// do not change the code be
+function isSameType(value1, value2) {
+    // Check if both inputs are NaN
+    if (isNaN(value1) && isInteger(Number(value2))) {
+        return false;
+    }
+    // Check if the types of the values are the same
+    else if (typeof value1 === typeof value2) {
+        return true;
+    }
+    // If none of the conditions are met, return false
+    return false;
+}
 
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
